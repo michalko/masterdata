49e2cb9f92e7068e6a0b303ecab76cdf74b3e23f
michalko <michal.lewandowski@gmail.com>
2020-05-31 00:07:11 +0200

    init session

--- src/main/java/com/brain1/masterdata/repos/TopicRepo.java
+++ src/main/java/com/brain1/masterdata/repos/TopicRepo.java
@@ -1,6 +1,7 @@
 package com.brain1.masterdata.repos;
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.transaction.Transactional;
 
@@ -11,9 +12,10 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface TopicRepo extends CrudRepository<Topic, Long> {
-
     @Transactional
     @Modifying
     @Query("update Topic t set t.activeCurrently = t.activeCurrently + 1 where t.name in (?1)")
     void incrementActiveForTopics(List<String> names);
+
+    Optional<Topic> findByName(String name);
 }
