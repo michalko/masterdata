427981a5f3ef7bacd9f0f0271a96d2934d39c181
michalko <michal.lewandowski@gmail.com>
2020-06-08 10:09:43 +0200

    cleaning

--- src/main/java/com/brain1/masterdata/DemoApplication.java
+++ src/main/java/com/brain1/masterdata/DemoApplication.java
@@ -1,14 +1,9 @@
 package com.brain1.masterdata;
 
-import java.util.function.Consumer;
-
-import com.brain1.masterdata.repos.TagRepo;
 import com.brain1.masterdata.repos.TopicRepo;
-import com.brain1.masterdata.repos.TopicTagsRepo;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
